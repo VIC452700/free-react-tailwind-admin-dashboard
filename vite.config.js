974d96d20b7b7f16e9b8a41cfb884c2d31f32902
daniel <vic452700@gmail.com>
2023-09-18 09:05:07 +0900

    update host url

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// export default defineConfig({
+//   server: {
+//     proxy: {
+//       "/test": {
+//         target: "http://localhost:5000",
+//         secure: false,
+//       },
+//     },
+//   },
+//   plugins: [react()],
+// });
+
 export default defineConfig({
   server: {
     proxy: {
       "/test": {
-        target: "http://localhost:5000",
+        target: "https://defivualt.onrender.com",
         secure: false,
       },
     },
